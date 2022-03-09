253b2c6ab99316e6294ebbfe4edb16a287f88654
Mateus <mateus.mello.estudante@eteot.rio.br>
2022-03-09 18:12:10 -0300

    Create index.php

---
+++ Aula/index.php
@@ -0,0 +1,96 @@
+<?php
+$a = 1;
+$b = 2;
+$c = 3;
+$d = 4;
+$e = 5;
+$f = 6;
+$g = 7;
+$h = 8;
+$j = 9;
+$k = 10;
+
+echo "<table border='1' cellpadding='10'>";
+
+echo "<tr>";
+
+echo "<td>";
+for ($i=1; $i <=10 ; $i++) { 
+    echo $a." x ".$i. " = ".$a*$i."<br>";
+}
+echo "</td>";
+
+echo "<td>";
+
+for ($i=1; $i <=10 ; $i++) { 
+    echo $b." x ".$i. " = ".$b*$i."<br>";
+}
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+
+echo "<td>";
+for ($i=1; $i <=10 ; $i++) { 
+    echo $c." x ".$i. " = ".$c*$i."<br>";
+}
+echo "</td>";
+
+echo "<td>";
+for ($i=1; $i <=10 ; $i++) { 
+    echo $d." x ".$i. " = ".$d*$i."<br>";
+}
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+
+echo "<td>";
+for ($i=1; $i <=10 ; $i++) { 
+    echo $e." x ".$i. " = ".$e*$i."<br>";
+}
+echo "</td>";
+
+echo "<td>";
+for ($i=1; $i <=10 ; $i++) { 
+    echo $f." x ".$i. " = ".$f*$i."<br>";
+}
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+
+echo "<td>";
+for ($i=1; $i <=10 ; $i++) { 
+    echo $g." x ".$i. " = ".$g*$i."<br>";
+}
+echo "</td>";
+
+echo "<td>";
+for ($i=1; $i <=10 ; $i++) { 
+    echo $h." x ".$i. " = ".$h*$i."<br>";
+}
+echo "</td>";
+echo "</tr>";
+
+echo "<tr>";
+
+echo "<td>";
+for ($i=1; $i <=10 ; $i++) { 
+    echo $j." x ".$i. " = ".$j*$i."<br>";
+}
+echo "</td>";
+
+echo "<td>";
+for ($i=1; $i <=10 ; $i++) { 
+    echo $k." x ".$i. " = ".$k*$i."<br>";
+}
+echo "</td>";
+echo "</tr>";
+echo "</table>";
+
+echo ""
+
+
+
+?>
